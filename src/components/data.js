d9168904651660dcd041c38b1099791693bb3e3c
Gustavo <gustavoguimaraescamps@gmail.com>
2022-09-30 03:24:34 -0300

    site funcionando

---
+++ src/components/data.js
@@ -0,0 +1,13 @@
+const data = [
+    {question: "O que é jsx?", answer: "Uma extensão de linguagem do JavaScript", results:""},
+    {question: " O React é __", answer: " uma biblioteca JavaScript para construção de interfaces", results:""},
+    {question: " Componentes devem iniciar com __ ", answer: "letra maiúscula", results:""},
+    {question: "Podemos colocar __ dentro do JSX", answer: "expressões", results:""},
+    {question: "O ReactDOM nos ajuda __ ", answer: " interagindo com a DOM para colocar componentes React na mesma", results:""},
+    {question: " Usamos o npm para __ ", answer: "gerenciar os pacotes necessários e suas dependências", results:""},
+    {question: " Usamos props para __ ", answer: "passar diferentes informações para componentes ", results:""},
+    {question: " Usamos estado (state) para __", answer: "dizer para o React quais informações quando atualizadas devem renderizar a tela novamente", results:""}
+    
+];
+
+ export default data
